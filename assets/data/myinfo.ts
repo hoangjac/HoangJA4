16db21490774422ebff8c34423105ff1d7c3d6b1
Jack Hoang <44513509+hoangjac@users.noreply.github.com>
2020-08-07 14:54:48 -0400

    Re-deploy to fix deployment issues

---
+++ assets/data/myinfo.ts
@@ -0,0 +1,11 @@
+import {HoangJac} from '../../app/hoangjac';
+import { title } from 'process';
+
+export const MYINFO: HoangJac = {
+    id: '991495093',
+    name: 'Jack Hoang',
+    login: 'hoangjac',
+    campus: 'Davis Campus',
+    title: 'Assignment 4'
+
+}
